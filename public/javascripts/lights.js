cbf867f8623a9de2bbcf079e5953f2811c7cc2cf
Anthony Shaffer <anthony@i13k.com>
2012-06-14 16:30:27 +0000

    wireup done but needs debugging - does not work yet

--- public/javascripts/lights.js
+++ public/javascripts/lights.js
@@ -1,159 +1,117 @@
 
     $(document).ready(function() { 
-      var port1Status = false;
-      var port2Status = false;
-      var port3Status = false;
-      var port4Status = false;
-
-      setPower = function() {
-        light1 = Number($("#light1-on").hasClass("btn-success"));
-        light2 = Number($("#light2-on").hasClass("btn-success"));
-        light3 = Number($("#light3-on").hasClass("btn-success"));
-        light4 = Number($("#light4-on").hasClass("btn-success"));
-
-        /*
-        urlGet = 'http://172.16.17.245/Set.cmd?CMD=SetPower+P61=' + light1 + '+P62=' + light2 + '+P63=' + light3 + '+P64=' + light4;
+      
+      getPower = function(callback) {
+        console.log("getPower");   
+        $.ajax({
+          url: '/lights',
+          method: 'GET',
+          dataType: 'json',
+          username: 'admin',
+          password: '12345678',
+          success: function(val) { 
+            console.log("getPower success"
+            console.log(val);
+            callback(val);
+          },
+          error: function(xhr, ajaxOptions, thrownError) { 
+            console.log("getPower got error");
+            console.log(xhr); 
+            console.log(ajaxOptions); 
+            callback(null); 
+          }
+        });
+      }
+      
+      setPower = function(port, state) {
+        console.log("setPower " + port + ' ' + state);   
+        urlGet = '/lights/' + port + '/' + state;
         console.log(urlGet);
         $.ajax({
           url: urlGet,
           method: 'GET',
-          dataType: 'jsonp',
-          beforeSend: function (xhr) { xhr.setRequestHeader ('Authorization', 'Basic YWRtaW46MTIzNDU2Nzg='); }, 
+          dataType: 'json',
+          username: 'admin',
+          password: '12345678',
           success: function(val) { 
-              var portStatusRead = null;
-              console.log(val.toString());
+            console.log("setPower success");
+            console.log(val);
           },
-          error: function(xhr, ajaxOptions, thrownError) { console.log(xhr); console.log(ajaxOptions); alert(thrownError); }
+          error: function(xhr, ajaxOptions, thrownError) { 
+            console.log("setPower got error");
+            console.log(xhr); 
+            console.log(ajaxOptions); 
+            alert(thrownError); 
+          }
         });
-        */
       }
-
+      
+      getPowerCallback = function(lightStatus) {
+        if (lightStatus == null) {
+          alert('could not get light status');
+        }
+        for (i=1; i < 5; i++) {
+          var jQuerySelector = '#light' + i;
+          if (lightStatus[i] == true)
+            $(jQuerySelector + "-on").addClass("btn-success");
+            $(jQuerySelector + "-off").removeClass("btn-danger");
+          else
+            $(jQuerySelector + "-on").removeClass("btn-success");
+            $(jQuerySelector + "-off").addClass("btn-danger");            
+        }
+      });
+      
+      // getPower(getPowerCallback);
+      
       $("#light1-on").click(function() {
           $("#light1-on").addClass("btn-success");
           $("#light1-off").removeClass("btn-danger");
-          setPower();
+          setPower(1,1);
       });
 
       $("#light1-off").click(function() {
           $("#light1-on").removeClass("btn-success");
           $("#light1-off").addClass("btn-danger");
-          setPower();
+          setPower(1,0);
       });
 
       $("#light2-on").click(function() {
           $("#light2-on").addClass("btn-success");
           $("#light2-off").removeClass("btn-danger");
-          setPower();
+          setPower(2,1);
       });
 
       $("#light2-off").click(function() {
           $("#light2-on").removeClass("btn-success");
           $("#light2-off").addClass("btn-danger");
-          setPower();
+          setPower(2,0);
       });
 
       $("#light3-on").click(function() {
           $("#light3-on").addClass("btn-success");
           $("#light3-off").removeClass("btn-danger");
-          setPower();
+          setPower(3,1);
       });
 
       $("#light3-off").click(function() {
           $("#light3-on").removeClass("btn-success");
           $("#light3-off").addClass("btn-danger");
-          setPower();
+          setPower(3,0);
       });
 
       $("#light4-on").click(function() {
           $("#light4-on").addClass("btn-success");
           $("#light4-off").removeClass("btn-danger");
-          setPower();
+          setPower(4,1);
       });
 
       $("#light4-off").click(function() {
           $("#light4-on").removeClass("btn-success");
           $("#light4-off").addClass("btn-danger");
-          setPower();
+          setPower(4,0);
       });
 
       $("#reset").click(function() {
-        /*
-        $.ajax({
-          url: 'http://172.16.17.245/Set.cmd?CMD=GetPower',
-          method: 'GET',
-          dataType: 'jsonp',
-          beforeSend: function (xhr) { xhr.setRequestHeader ('Authorization', 'Basic YWRtaW46MTIzNDU2Nzg='); }, 
-          success: function(val) { 
-              // val = <html>p61=0,p62=0,p63=0,p64=0</html>
-              var portStatusRead = null;
-              console.log(val.toString());
-
-              portStatusRead = val.match('p61=1'); // found the string that indicates that the port is one, otherwise "null"
-              if (portStatusRead == null) // light is off
-              {
-                 port1Status = false; // update status to false to indicate that the light is off
-                 $("#light1-on").removeClass("btn-success");
-                 $("#light1-off").addClass("btn-danger");
-              }
-              else // the string p61=1 was found in the status info from the light box, therefore it must be on
-              {
-                port1Status = true; // update status to true to indicate 
-                $("#light1-on").addClass("btn-success");
-                $("#light1-off").removeClass("btn-danger");
-
-              }
-
-              portStatusRead = val.match('p62=1'); // found the string that indicates that the port is one, otherwise "null"
-              if (portStatusRead == null) // light is off
-              {
-                 port2Status = false; // update status to false to indicate that the light is off
-                 $("#light2-on").removeClass("btn-success");
-                 $("#light2-off").addClass("btn-danger");
-              }
-              else // the string p61=1 was found in the status info from the light box, therefore it must be on
-              {
-                port2Status = true; // update status to true to indicate 
-                $("#light2-on").addClass("btn-success");
-                $("#light2-off").removeClass("btn-danger");
-
-              }
-
-              portStatusRead = val.match('p63=1') ;// found the string that indicates that the port is one, otherwise "null"
-              if (portStatusRead == null) // light is off
-              {
-                 port3Status = false; // update status to false to indicate that the light is off
-                 $("#light3-on").removeClass("btn-success");
-                 $("#light3-off").addClass("btn-danger");
-              }
-              else // the string p61=1 was found in the status info from the light box, therefore it must be on
-              {
-                port3Status = true; // update status to true to indicate 
-                $("#light3-on").addClass("btn-success");
-                $("#light3-off").removeClass("btn-danger");
-              }
-
-              portStatusRead = val.match('p64=1'); // found the string that indicates that the port is one, otherwise "null"
-              if (portStatusRead == null) // light is off
-              {
-                 port4Status = false; // update status to false to indicate that the light is off
-                 $("#light4-on").removeClass("btn-success");
-                 $("#light4-off").addClass("btn-danger");
-              }
-              else // the string p61=1 was found in the status info from the light box, therefore it must be on
-              {
-                port4Status = true; // update status to true to indicate 
-                $("#light4-on").addClass("btn-success");
-                $("#light4-off").removeClass("btn-danger");
-              }
-              console.log(port1Status)
-              if (port1Status == true)
-                 statusText = "Light is On"
-              $("#statusText1").val(port1Status);
-              $("#statusText2").val(port2Status);
-              $("#statusText3").val(port3Status);
-              $("#statusText4").val(port4Status);
-          },
-          error: function(xhr, ajaxOptions, thrownError) { console.log(xhr); console.log(ajaxOptions); alert(thrownError); }
-        });*/
+          getPower(getPowerCallback);
       });
     });
--- server.js
+++ server.js
@@ -13,17 +13,17 @@
 ============================================================================= */
 
 var express = require('express'),
-		inspect = require('inspect'),
-		colors = require('colors'),
-		sys = require('util'),
-		mime = require('mime'),
-		cleanCSS = require('clean-css'),
-		httpProxy = require('http-proxy'),
+    inspect = require('inspect'),
+    colors = require('colors'),
+    sys = require('util'),
+    mime = require('mime'),
+    cleanCSS = require('clean-css'),
+    httpProxy = require('http-proxy'),
     rest = require('restler'),
     rand = require('mersenne').rand,
     less = require('less'),
     lessMiddleware = require('less-middleware'),
-		app = module.exports = express.createServer();
+    app = module.exports = express.createServer();
 
 
 /*===========================================================================
@@ -31,33 +31,29 @@ var express = require('express'),
 ============================================================================= */
 
 var port = process.env.PORT || 3000,
-		cacheAge = 60000 * 60 * 24 * 365,
-		logs = {
-			set: false,
-			string: '\\n  ' + ':date'.bold.underline + '\\n\\n' + '  IP: '.cyan.bold
-				+ ' ' + ':remote-addr'.white + '\\n' + '  Method: '.red.bold
-				+ ':method'.white + '\\n' + '  URL: '.blue.bold + ':url'.white
-				+ '\\n' + '  Status: '.yellow.bold + ':status'.white + '\\n'
-				+ '  User Agent: '.magenta.bold + ':user-agent'.white
-		};
+    cacheAge = 60000 * 60 * 24 * 365,
+    logs = {
+    set: false,
+    string: '\\n  ' + ':date'.bold.underline + '\\n\\n' + '  IP: '.cyan.bold + ' ' + ':remote-addr'.white + '\\n' + '  Method: '.red.bold + ':method'.white + '\\n' + '  URL: '.blue.bold + ':url'.white + '\\n' + '  Status: '.yellow.bold + ':status'.white + '\\n' + '  User Agent: '.magenta.bold + ':user-agent'.white
+    };
 
 
 /*===========================================================================
 	DEFAULT CONFIG
 ============================================================================= */
 
-app.configure(function(){
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'ejs');
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.favicon(__dirname + '/public/favicon.ico'));
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.favicon(__dirname + '/public/favicon.ico'));
   app.use(lessMiddleware({
-        src: __dirname + '/public',
-        compress: true
-    }));	
+    src: __dirname + '/public',
+    compress: true
+  }));
   app.use(app.router);
-	if(logs.set) app.use(express.logger(logs.string));
+  if (logs.set) app.use(express.logger(logs.string));
 });
 
 
@@ -67,12 +63,12 @@ app.configure(function(){
 ============================================================================= */
 
 app.configure('development', function() {
-	
-	app.use(express.static(__dirname + '/public'));
-	app.use(express.errorHandler({
-		dumpExceptions : true,
-		showStack      : true
-	}));
+
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
 });
 
 
@@ -82,9 +78,11 @@ app.configure('development', function() {
 ============================================================================= */
 
 app.configure('production', function() {
-	port = 8080;
-	app.use(express.static(__dirname + '/public', { maxAge: cacheAge }));
-	app.use(express.errorHandler());
+  port = 8080;
+  app.use(express.static(__dirname + '/public', {
+    maxAge: cacheAge
+  }));
+  app.use(express.errorHandler());
 });
 
 
@@ -95,16 +93,16 @@ app.configure('production', function() {
 // Since this is the last non-error-handling
 // middleware use()d, we assume 404, as nothing else
 // responded.
-app.use(function(req, res, next){
-	// the status option, or res.statusCode = 404
-	// are equivalent, however with the option we
-	// get the "status" local available as well
-	res.render('404', {
-		layout: false,
-		status: 404,
-		//url: req.url,
-		title: '404 Error'
-	});
+app.use(function(req, res, next) {
+  // the status option, or res.statusCode = 404
+  // are equivalent, however with the option we
+  // get the "status" local available as well
+  res.render('404', {
+    layout: false,
+    status: 404,
+    //url: req.url,
+    title: '404 Error'
+  });
 });
 
 // error-handling middleware, take the same form
@@ -112,23 +110,21 @@ app.use(function(req, res, next){
 // arity of 4, aka the signature (err, req, res, next).
 // when connect has an error, it will invoke ONLY error-handling
 // middleware.
-
 // If we were to next() here any remaining non-error-handling
 // middleware would then be executed, or if we next(err) to
 // continue passing the error, only error-handling middleware
 // would remain being executed, however here
 // we simply respond with an error page.
-
-app.use(function(err, req, res, next){
-	// we may use properties of the error object
-	// here and next(err) appropriately, or if
-	// we possibly recovered from the error, simply next().
-	res.render('500', {
-		layout: false,
-		status: err.status || 500,
-		error: err,
-		title: '500 Error'
-	});
+app.use(function(err, req, res, next) {
+  // we may use properties of the error object
+  // here and next(err) appropriately, or if
+  // we possibly recovered from the error, simply next().
+  res.render('500', {
+    layout: false,
+    status: err.status || 500,
+    error: err,
+    title: '500 Error'
+  });
 });
 
 
@@ -138,25 +134,25 @@ app.use(function(err, req, res, next){
 
 // Default from HTML5 Boilerplate 
 app.get('/demo', function(req, res) {
-	res.render('index', {
-		modernizr: "javascripts/libs/modernizr-2.0.6.min.js",
-		jquery: "javascripts/libs/jquery-1.7.2.min.js",
-		title: 'this is a title',
-		description: 'this is a description',
-		javascripts: ["javascripts/script.js"],
-		stylesheets: ["style.css"]
-	});
+  res.render('index', {
+    modernizr: "javascripts/libs/modernizr-2.0.6.min.js",
+    jquery: "javascripts/libs/jquery-1.7.2.min.js",
+    title: 'this is a title',
+    description: 'this is a description',
+    javascripts: ["javascripts/script.js"],
+    stylesheets: ["style.css"]
+  });
 });
 
 app.get('/', function(req, res) {
   res.render('lights', {
-		modernizr: "javascripts/libs/modernizr-2.0.6.min.js",
-		jquery: "javascripts/libs/jquery-1.7.2.min.js",
-		title: 'this is a title',
-		description: 'this is a description',
-		javascripts: ["javascripts/script.js", "javascripts/lights.js"],
-		stylesheets: ["style.css"]
-	});
+    modernizr: "javascripts/libs/modernizr-2.0.6.min.js",
+    jquery: "javascripts/libs/jquery-1.7.2.min.js",
+    title: 'this is a title',
+    description: 'this is a description',
+    javascripts: ["javascripts/script.js", "javascripts/lights.js"],
+    stylesheets: ["style.css"]
+  });
 });
 
 app.get('/mu-*', function(req, res) {
@@ -169,6 +165,11 @@ app.get('/mu-*', function(req, res) {
 ============================================================================= */
 
 app.get('/lights', function(req, res) {
+  console.log('/lights - test only');
+  res.json(["html", Boolean(rand(1)), Boolean(rand(1)), Boolean(rand(1)), Boolean(rand(1))]);
+});
+
+app.get('/lights-real', function(req, res) {
   console.log('/lights');
   rest.get('http://web1.tunnlr.com:12375/Set.cmd', {
     username: 'admin',
@@ -179,12 +180,12 @@ app.get('/lights', function(req, res) {
   }).on('complete', function(result, response) {
     if (result instanceof Error) {
       console.log('Error: ' + result.message);
-      res.send (400, "Failed to GetPower"); 
+      res.send(400, "Failed to GetPower");
       return;
-    } 
-    
+    }
+
     console.log(result);
-    
+
     var lights = null;
     var matchResults = result.match('......p61=(.)[,]p62=(.)[,]p63=(.)[,]p64=(.).....*');
     console.log(matchResults);
@@ -198,21 +199,28 @@ app.get('/lights', function(req, res) {
       };
     }
     console.log(lights);
-		res.json(lights);
-	});
+    res.json(lights);
+  });
+});
+
+app.get('/lights/:port/:state', function(req, res) {
+  result = '<html>p61=1</html>'
+  lightStatus = result.match('.......[6](.)=(.).....*');
+  console.log(lightStatus);
+  res.json(lightStatus);
 });
 
 app.get('/lights/:port/:state', function(req, res) {
   console.log('/lights/:port/:state');
-  if(req.params.port === 'x') {
+  if (req.params.port === 'x') {
     req.params.port = rand(4) + 1;
     console.log("req.params.port = (random) " + String(req.params.port));
   }
-  if(req.params.state === 'x') {
-    req.params.state = rand(2); 
+  if (req.params.state === 'x') {
+    req.params.state = rand(2);
     console.log("req.params.state = (random) " + String(req.params.state));
   }
-  query = "P6"+ req.params.port + "=" + req.params.state;
+  query = "P6" + req.params.port + "=" + req.params.state;
   url = 'http://web1.tunnlr.com:12375/Set.cmd?CMD=SetPower+' + query;
   console.log(url);
   rest.get(url, {
@@ -221,20 +229,14 @@ app.get('/lights/:port/:state', function(req, res) {
   }).on('complete', function(result, response) {
     if (result instanceof Error) {
       console.log('Error: ' + result.message);
-      res.send (400, "Failed to GetPower"); 
+      res.send(400, "Failed to GetPower");
       return;
-    } 
-    
-    console.log(result);
-    var lights = null;
-    lightStatus = result.match('......p6(.)=(.).....*');
-    if (lightStatus && lightStatus.length > 2) {
-      lights[lightStatus[1]] = lightStatus[2]
     }
-    console.log(lights);
-    
-  	res.json(lights);
-	});
+    console.log(result);
+    lightStatus = result.match('.......[6](.)=(.).....*');
+    console.log(lightStatus);
+    res.json(lightStatus);
+  });
 });
 
 
@@ -243,10 +245,10 @@ app.get('/lights/:port/:state', function(req, res) {
 ============================================================================= */
 var proxy = new httpProxy.RoutingProxy();
 app.get('/ippower', function(req, res) {
-	proxy.proxyRequest(req, res,  {
-		host: 'ippower.dillflies.com',
-		port: 12375
-	});
+  proxy.proxyRequest(req, res, {
+    host: 'ippower.dillflies.com',
+    port: 12375
+  });
 });
 
 /*===========================================================================
@@ -278,76 +280,65 @@ app.get('/500', function(req, res, next){
 
 function wildcard(req, res, next) {
 
-	/*=========================================================================
+/*=========================================================================
 		BETTER WEBSITE EXPERIENCE FOR IE USERS
 	=========================================================================== */
-	var url = req.url,
-			ua = req.headers['user-agent'],
-			reqPath = req.url;
+  var url = req.url,
+      ua = req.headers['user-agent'],
+      reqPath = req.url;
 
-	if(ua && ua.indexOf('MSIE'))
-		res.setHeader('X-UA-Compatible', 'IE=Edge,chrome=1');
+  if (ua && ua.indexOf('MSIE')) res.setHeader('X-UA-Compatible', 'IE=Edge,chrome=1');
 
-	// We don't want to send this header on _everything_
-	if(reqPath.match(/\.(js|css|gif|png|jpe?g|pdf|xml|oga|ogg|m4a|ogv|mp4|m4v|webm|svg|svgz|eot|ttf|otf|woff|ico|webp|appcache|manifest|htc|crx|xpi|safariextz|vcf)$/)) {
-		res.removeHeader('X-UA-Compatible');
-		res.removeHeader('IE=Edge,chrome=1');
-	}
+  // We don't want to send this header on _everything_
+  if (reqPath.match(/\.(js|css|gif|png|jpe?g|pdf|xml|oga|ogg|m4a|ogv|mp4|m4v|webm|svg|svgz|eot|ttf|otf|woff|ico|webp|appcache|manifest|htc|crx|xpi|safariextz|vcf)$/)) {
+    res.removeHeader('X-UA-Compatible');
+    res.removeHeader('IE=Edge,chrome=1');
+  }
 
-	/*=========================================================================
+/*=========================================================================
 		CORS
 	=========================================================================== */
-	// Force the latest IE version, in cases when it may fall back to IE7 mode
-	// http://github.com/rails/rails/commit/123eb25#commitcomment-118920
-	// Use ChromeFrame if it's installed, for a better experience with IE folks
-	// Control cross domain using CORS http://enable-cors.org
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+  // Force the latest IE version, in cases when it may fall back to IE7 mode
+  // http://github.com/rails/rails/commit/123eb25#commitcomment-118920
+  // Use ChromeFrame if it's installed, for a better experience with IE folks
+  // Control cross domain using CORS http://enable-cors.org
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
 
-	/*=========================================================================
+/*=========================================================================
 		MIME TYPES
 	=========================================================================== */
 
-	// Until pull request is in
-	// https://github.com/bentomas/node-mime
-	mime.define({
-		'font/opentype'                  : ['otf'],
-		'text/cache-manifest'            : ['appcache', 'manifest'],
-		'text/x-component'               : ['htc'],
-		'application/x-chrome-extension' : ['crx'],
-	});
+  // Until pull request is in
+  // https://github.com/bentomas/node-mime
+  mime.define({
+    'font/opentype': ['otf'],
+    'text/cache-manifest': ['appcache', 'manifest'],
+    'text/x-component': ['htc'],
+    'application/x-chrome-extension': ['crx'],
+  });
 
-	// TODO: Allow concatenation from within specific js and css files
-	// https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L111
-
-	// TODO: Gzip compression
-	// https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L137
-
-	// TODO: Expires headers (for better cache control)
-	// https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L186
-
-	// TODO: Stop screen flicker in IE on CSS rollovers
-	// https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L271
-
-	// TODO: Cookie setting from iframes
-	// https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L286
-
-	// TODO: Suppress or force the "www." at the beginning of URLs
-	// https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L315
-
-	// TODO: Built-in filename-based cache busting
-	// https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L356
-
-	// TODO: Prevent SSL cert warnings
-	// https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L376
-
-	// TODO: UTF-8 encoding
-	// https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L411
-
-	// TODO: Block access to "hidden" directories whose names begin with a period
-	// https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L442
-
-	next();
+  // TODO: Allow concatenation from within specific js and css files
+  // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L111
+  // TODO: Gzip compression
+  // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L137
+  // TODO: Expires headers (for better cache control)
+  // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L186
+  // TODO: Stop screen flicker in IE on CSS rollovers
+  // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L271
+  // TODO: Cookie setting from iframes
+  // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L286
+  // TODO: Suppress or force the "www." at the beginning of URLs
+  // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L315
+  // TODO: Built-in filename-based cache busting
+  // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L356
+  // TODO: Prevent SSL cert warnings
+  // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L376
+  // TODO: UTF-8 encoding
+  // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L411
+  // TODO: Block access to "hidden" directories whose names begin with a period
+  // https://github.com/paulirish/html5-boilerplate/blob/master/.htaccess/#L442
+  next();
 }
 
 /*===========================================================================
@@ -361,17 +352,6 @@ app.use(wildcard);
 
 app.listen(port);
 var appPort = app.address().port + '',
-		appEnv = app.settings.env.toUpperCase();
+    appEnv = app.settings.env.toUpperCase();
 
-sys.puts(''
-	+ 'SERVER LISTENING ON PORT '.rainbow
-	+ " ".white.inverse
-	+ appPort.white.inverse
-	+ " ".white.inverse
-	+ ' IN '.rainbow
-	+ " ".white.inverse
-	+ appEnv.white.inverse
-	+ " ".white.inverse
-	+ ' MODE '.rainbow
-);
-
+sys.puts('' + 'SERVER LISTENING ON PORT '.rainbow + " ".white.inverse + appPort.white.inverse + " ".white.inverse + ' IN '.rainbow + " ".white.inverse + appEnv.white.inverse + " ".white.inverse + ' MODE '.rainbow);
